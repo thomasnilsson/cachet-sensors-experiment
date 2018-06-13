ac772822a9289f91b80411feecfa4a7435d09ccb
thomasnilsson <s144470@student.dtu.dk>
2018-06-13 15:41:40 +0200

    Added flutter pedometer back in

---
+++ Pedometer/FlutterPedometer/build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/armv7/GeneratedPluginRegistrant.d
@@ -0,0 +1,16 @@
+dependencies: \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterNavigationController.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/thomasnilsson/Desktop/FlutterPedometer/ios/Flutter/Flutter.framework/Headers/Flutter.h
